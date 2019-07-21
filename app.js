963f131238c3479282eabfbe4fe05d8c8e8bd095
mypichayada01 <52844312+mypichayada01@users.noreply.github.com>
2019-07-21 15:02:01 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import createError from "http-errors";
+import express from "express";
+import session from "express-session";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import {
+  homeRouter,
+  authRouter,
+  userRouter,
+  adminRouter,
+  apiRouter
+} from "./routes";
+import config from "./config";
+import csrfToken from "csurf";
+import flash from "express-flash";
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(
+  session({
+    secret: config.sessionSecret,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+  })
+);
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//app.use(csrfToken);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.auth = req.session.auth;
+  next();
+});
+
+const adminOnlyMiddleware = (req, res, next) => {
+  if (!req.session.auth) {
+    return res.redirect("/auth/logout");
+  }
+  next();
+};
+
+//routes
+app.use("/", homeRouter);
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
+app.use("/admin", adminOnlyMiddleware, adminRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +67,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
